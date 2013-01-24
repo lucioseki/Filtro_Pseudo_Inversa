3667863c3c541887e153f458f26189e78d8517ed
Lucio Mitsuru Seki <lucioseki@gmail.com>
2013-01-23 23:09:50 -0200

    2)a) Apliquei o filtro de minima norma sem restrições (pseudo-inversa).

--- pseudo_inversa.m
+++ pseudo_inversa.m
@@ -31,7 +31,7 @@ figure(1), colormap(gray(256)), image(fpad), title("fpad");
 
 # variancia = desvio padrao ^ 2
 
-variancia = 1;
+variancia = 0.5;
 left = 1 / ((2 * pi * variancia)**0.5);
 expdiv = 2 * variancia;
 
@@ -75,3 +75,35 @@ n = 20 * n;
 g = zeros(x * 2, y * 2);
 g([x/2 + 1 : x/2 + x], [y/2 + 1 : y/2 + y]) = gconv([x/2 + 1 : x/2 + x], [y/2 + 1 : y/2 + y]) .+ n;
 figure(3), colormap(gray(256)), image(g), title("g");
+
+# ------------------------------------------------------------
+# RESTAURACAO
+# ------------------------------------------------------------
+
+# operacao com as dimensoes da imagem original, para diminuir o custo computacional
+
+# H = funcao de transferencia (TF de h)
+H = fft2(hpad([x/2 + 1:x/2 + x],[y/2 + 1:y/2 + y]));
+
+# G = TF de g
+G = fft2(g([x/2 + 1:x/2 + x],[y/2 + 1:y/2 + y]));
+
+# F_ = imagem estimada no dominio da frequencia
+F_ = G ./ H;
+
+# f_ = imagem estimada no dominio do espaco
+f_ = ifft2(F_);
+
+# normalizacao para ajustar a 256 tons de cinza
+tommax = max(max(real(f_)));
+tommin = min(min(real(f_)));
+
+f_norm = 256*(real(f_) - tommin)/(tommax-tommin);
+figure(4), colormap(gray(256)), image(real(f_norm)), title("inversa aplicada");
+
+# a convolucao opera com posicoes negativas da matriz h
+# por isso o resultado vem deslocado
+# desloco novamente para corrigir
+f_shift = fftshift(f_norm);
+figure(5), colormap(gray(256)), image(real(f_shift)), title("shift sobre a inversa");
+
