c337d00b8d32ebbe9b5a1b4f747817501c8d6d54
Lucio Mitsuru Seki <lucioseki@gmail.com>
2013-01-23 22:40:47 -0200

    1)b) Construí a função de espalhamento pontual.

--- pseudo_inversa.m
+++ pseudo_inversa.m
@@ -24,3 +24,20 @@ fpad = zeros(x * 2, y * 2);
 fpad([x/2 + 1 : x/2 + x], [y/2 + 1 : y/2 + y]) = f;
 
 figure(1), colormap(gray(256)), image(fpad), title("fpad");
+
+# ------------------------------------------------------------
+# Gaussiana
+# ------------------------------------------------------------
+
+# variancia = desvio padrao ^ 2
+
+variancia = 1;
+left = 1 / ((2 * pi * variancia)**0.5);
+expdiv = 2 * variancia;
+
+h = zeros(x, y);
+for i = 1 : x
+	for j = 1 : y
+		h(i, j) = left * e**(-( ((i - x / 2)**2 + (j - y / 2)**2) / expdiv ));
+	end
+end
